078ebbb9242e3a007690842e52729bfde911c092
Tony Bragin <antonbragin28@yandex.ru>
2020-01-05 17:30:55 +0700

    add xcode project

---
+++ StructsPresenter/StructsPresenter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StructsPresenter
+//
+//  Created by Tony on 05/01/2020.
+//  Copyright © 2020 Tony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
